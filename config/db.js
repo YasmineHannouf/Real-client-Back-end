c254624141e8276f9b2bd1fbb9c90e63432eab99
mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-04-09 13:44:01 +0300

    add connect db

---
+++ config/db.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to mongoDB");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
+
+// Connection error handling
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDb disconnected!");
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDb connected!");
+})
+
+export default connectDB;
---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+const app = express();
+
+import cookieParser from "cookie-parser";
+
+// import and config dotenv
+import dotenv from "dotenv";
+dotenv.config();
+
+// import db connection
+import connectDB from "./config/db.js";
+
+// import routes
+
+
+
+connectDB();
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+    const errStatus = err.status || 500;
+    const errMessage = err.message || "Something went wrong";
+    return res.status(errStatus).json({
+      success: false,
+      message: errMessage,
+      stack: err.stack,
+    });
+  });
+
+app.listen(5000, () => {
+    console.log("listening on port 5000");
+  });
