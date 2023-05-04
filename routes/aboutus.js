b7d151b6a014eb4b40ec4d5d6f0e418b2125b262
abdelrahman <abdelrahman.assoum@gmail.com>
2023-05-04 09:47:32 +0300

    fix about us

--- routes/aboutus.js
+++ routes/aboutus.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { getAboutusById, updateAboutus } from "../controllers/aboutus.js";
+import { getAbout, updateAboutus } from "../controllers/aboutus.js";
 import { singleImage } from "../middlewares/imageHandler.js";
 
 // Get a specific aboutus by ID
-router.get("/:id", getAboutusById);
+router.get("/", getAbout);
 
 // Update an existing aboutus by ID
 router.patch("/edit/:id", singleImage, updateAboutus);
